9397db61ff7dbb963620755f8ea4613e636f422a
owentanner <owen.w.tanner3@gmail.com>
2019-05-23 16:51:01 -0400

    Dynamic port assignment

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/about', (req, res) => {
   res.send('i like long walks on the beach');
 })
 
-
-app.listen(4000, () => console.log('listening on 4000'));
+const port = (process.env.PORT || 4000);
+app.listen(port, () => console.log(`listening on ${port}`));
